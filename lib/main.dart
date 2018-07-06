2532ddf791187e8c79ef3e937c1187198cee9d57
Unknown <pkheni@inboxmarketer.com>
2018-07-06 16:47:50 -0400

    add some content to steps and important todo

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_app/model/Categories.dart';
 
 void main() => runApp(new MyApp());
 
+//TODO Implement something that allows admin to add more checklists
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
